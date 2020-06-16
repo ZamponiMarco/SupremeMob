8d8511f84d7a2227457bdf82180e960df796d3be
ZamponiMarco <zamponi.marco3@gmail.com>
2020-06-16 20:00:31 +0200

    Initial commit

---
+++ starter-plugin-core/src/main/java/com/github/jummes/StarterPlugin.java
@@ -0,0 +1,11 @@
+package com.github.jummes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StarterPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
